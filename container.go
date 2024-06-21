d77977e2bb4b8aef52747e6300376ea6868b5991
iscanderoff <scanderoff@gmail.com>
2024-06-21 16:24:01 +0800

    Dict, Set and Queue interfaces

--- container.go
+++ container.go
@@ -8,3 +8,26 @@ type Container[E any] interface {
 
 	fmt.Stringer
 }
+
+type Dict[K comparable, V any] interface {
+	Get(key K) (val V, ok bool)
+	Set(key K, val V)
+
+	Container[V]
+}
+
+type Set[E comparable] interface {
+	Add(elems ...E)
+	Remove(elems ...E)
+	Contains(elem E) bool
+
+	Container[E]
+}
+
+type Queue[E any] interface {
+	Enqueue(elems ...E)
+	Dequeue() (elem E, ok bool)
+	Peek() (elem E, ok bool)
+
+	Container[E]
+}
