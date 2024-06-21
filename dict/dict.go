3e70b296cce453298fcc4da26373df093db69b31
iscanderoff <scanderoff@gmail.com>
2024-06-21 15:08:49 +0800

    Dict All iterator

--- dict/dict.go
+++ dict/dict.go
@@ -1,7 +1,19 @@
 package dict
 
+import "iter"
+
 type Dict[K comparable, V any] map[K]V
 
 func New[K comparable, V any](capacity int) Dict[K, V] {
 	return make(Dict[K, V], capacity)
+}
+
+func (d Dict[K, V]) All() iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range d {
+			if !yield(k, v) {
+				return
+			}
+		}
+	}
 }
